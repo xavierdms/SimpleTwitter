392a9d07ea4ec2530076a1de2f8dca540a6fed42
xavierdms <id5@humboldt.edu>
2019-02-21 16:50:03 -0800

    added compose button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivityNight.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivityNight.java
@@ -7,8 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -77,6 +80,25 @@ public class TimelineActivityNight extends AppCompatActivity {
                 ));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose)
+        {
+            // Tapped on the compose icon
+            Toast.makeText(this,"Compose!", Toast.LENGTH_SHORT).show();
+            // Navigate to a new activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
